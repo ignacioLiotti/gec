c8e67596e1b29fe6ba9c8afbf87a740d3d7a1d0f
ignacioLiotti <ignacioLiotti@users.noreply.github.com>
2026-04-08 12:11:48 -0300

    feat(excel): pin /excel landing to before mode

--- app/excel/page.tsx
+++ app/excel/page.tsx
@@ -1,20 +1,5 @@
-import { headers } from "next/headers";
-import ExcelPageClient from "./excel-page-client";
-import { getExcelPageInitialData } from "@/lib/excel/page-data";
-
-const MOBILE_USER_AGENT_PATTERN =
-  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile/i;
+import { renderExcelLandingPage } from "./landing-page";
 
 export default async function ExcelPage() {
-  const headerStore = await headers();
-  const userAgent = headerStore.get("user-agent") ?? "";
-  const { mainTableColumnsConfig, obras } = await getExcelPageInitialData();
-
-  return (
-    <ExcelPageClient
-      initialIsMobile={MOBILE_USER_AGENT_PATTERN.test(userAgent)}
-      initialMainTableColumnsConfig={mainTableColumnsConfig}
-      initialObras={obras}
-    />
-  );
+	return renderExcelLandingPage("before");
 }
